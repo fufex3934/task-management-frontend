4706b1c0fe005ddde48fc86f4bb189ab6beb3a3e
Fufa Wakgari <fufawakagari174@gmail.com>
2026-03-10 10:54:19 +0300

    create nav bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Navbar from "./components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <QueryClientProvider client={queryClient}>
+          <Navbar/>
           {children}
         </QueryClientProvider>
       </body>
